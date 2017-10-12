9d5e3b09d52a16a88430914c40820a561569e906
amatranga <amatranga892@gmail.com>
2017-10-12 13:56:39 -0700

    Create tests file

---
+++ __tests__/app.js
@@ -0,0 +1,30 @@
+'use strict';
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+
+describe('generator-tuba-react-ts:app', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ someAnswer: true });
+  });
+
+  it('creates public files', () => {
+    assert.file('public/index.html');
+  });
+  it('creates client files', () => {
+    assert.file(['client/src/index.tsx', 'client/src/components/App.tsx']);
+  });
+  it('creates server files', () => {
+    assert.file(['server/index.js', 'server/app.js']);
+  });
+  it('creates other root level files', () => {
+    assert.file([
+      'bower.json',
+      'package.json',
+      '.gitignore',
+      '.bowerrc',
+      'webpack.config.js'
+    ]);
+  });
+});
